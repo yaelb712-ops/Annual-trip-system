f6471cfd89c0128bba8b5a6d6ae249433ffbb91b
Yael B <yaelb7.12@gmail.com>
2026-04-22 12:33:32 +0300

    add models and db

---
+++ server/models.py
@@ -0,0 +1,28 @@
+from app import db
+
+class Students(db.Model):
+    __tablename__ = 'Students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(20), nullable=False)
+    lastName = db.Column(db.String(20), nullable=False)
+    identityNumber = db.Column(db.String(9), nullable=False, unique=True)
+    classN = db.Column(db.String(10), nullable=False)
+
+class Teachers(db.Model):
+    __tablename__ = 'Teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(20), nullable=False)
+    lastName = db.Column(db.String(20), nullable=False)
+    identityNumber = db.Column(db.String(9), nullable=False, unique=True)
+    classN = db.Column(db.String(10), nullable=False)
+
+class Location(db.Model):
+    __tablename__ = 'Locations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    studentIdentity = db.Column(db.String(9), nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    timeS = db.Column(db.DateTime, nullable=False)
